2b8770048ed788914b862aeeedf9a64f88b44e62
Daniel Rodriguez <dannyrodriguez822@gmail.com>
2014-12-02 09:19:29 -0800

    12/2/14

--- public_html/js/screens/title.js
+++ public_html/js/screens/title.js
@@ -1,22 +1,24 @@
 game.TitleScreen = me.ScreenObject.extend({
-	/**	
-	 *  action to perform on state change
-	 */
-	onResetEvent: function() {	
-	var titleImage = new me.Sprite(0, 0, me.loader.getImage("title-screen"));
+    /**	
+     *  action to perform on state change
+     */
+    onResetEvent: function() {
+        var titleImage = new me.Sprite(0, 0, me.loader.getImage("title-screen"));
         me.game.world.addChild(titleImage, 1);
-        
         me.input.bindKey(me.input.KEY.ENTER, "start");
+
+
         this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge){
-            
-        })
-	},
-	
-	
-	/**	
-	 *  action to perform when leaving this screen (state change)
-	 */
-	onDestroyEvent: function() {
-		; // TODO
-	}
+            if (action === "start") {
+                me.state.change(me.state.PLAY);
+            }
+        });
+
+    },
+    /**	
+     *  action to perform when leaving this screen (state change)
+     */
+    onDestroyEvent: function() {
+        ; // TODO
+    }
 });
